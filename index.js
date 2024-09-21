024ff883ceb4e2fb82ec9ebb25a8088414743fe3
behzad05 <39502078+behzad05@users.noreply.github.com>
2024-09-21 09:22:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("test 23");
 (function () {
     // Function to hide all target elements when they appear
     function hideTargetElements() {
+        console.log("function ran")
         const selectors = [
             "#main-app > div > div > div > div > div.grid.grid-cols-2.lg\\:grid-cols-4.gap-3.mb-6 > div:nth-child(1)",
             "#main-app > div > div > div > div > div.grid.grid-cols-2.lg\\:grid-cols-4.gap-3.mb-6 > div:nth-child(1)"
@@ -18,18 +19,7 @@ console.log("test 23");
         });
     }
 
-    // Create a Mutation Observer to watch for changes in the DOM
-    const observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function () {
-            hideTargetElements(); // Call the function whenever there's a change in the DOM
-        });
-    });
 
-    // Start observing the body for changes in child elements
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
 
     // Run the function once initially in case the elements are already present
     setInterval(hideTargetElements, 2000);
